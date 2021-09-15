18a64e2807f8b824fd153ce6a987e5fb3252ee1e
anshi97 <52496881+anshi97@users.noreply.github.com>
2021-09-15 18:07:14 +0530

    Update JavaProgram.java
    98

--- DemoProjectNew/src/com/sonar/io/JavaProgram.java
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -35,7 +35,7 @@ public class JavaProgram {
   
         System.out.println(new String(result)); 
         System.out.println(new String(result)); 
-        // Requirement ID : 86
+        // Requirement ID : 67
 	
 	}
 	
