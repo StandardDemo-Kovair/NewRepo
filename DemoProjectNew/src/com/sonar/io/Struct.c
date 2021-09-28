09271725d71f44f30b7fc0210db3ac07211bac65
anshi97 <52496881+anshi97@users.noreply.github.com>
2021-09-28 13:29:04 +0530

    Add files via upload

---
+++ DemoProjectNew/src/com/sonar/io/Struct.c
@@ -0,0 +1,268 @@
+<<<<<<< .mine
+
+ # Begin Testbed Set
+
+ SINGLE_FILE = TRUE
+ GENERATED_BY = Testbed 9.8.3
+ HOSTNAME = Imphal
+
+    # Begin Source Files
+
+    RelativeFile = .\Structs.c
+    File = C:\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c
+    UNCFile = \\Imphal\C\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c
+
+    # End Source Files
+
+    # Begin Sysearch Include File Entries
+
+
+    # End Sysearch Include File Entries
+
+    # Begin Sysppvar Preprocessor Macros
+
+
+    # End Sysppvar Preprocessor Macros
+
+    # Begin Options
+
+    set_attributes = 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    nb_mechanism = build
+    nb_start_in_dir = $(SourceDir)
+    nb_build_command = gcc "$$d$$f.$$e" -o "$$i$$x$$g"
+    nb_compile_command = gcc -c "$$d$$f.$$e"
+    nb_link_command = gcc $$d$$f.o -o "$$i$$x"
+    nb_ide_command = <Unknown TO GCC>
+    nb_makefile_command = mingw32-make -f "$(Makefile)" $(MakeGoal) $(MakeArgs)
+    nb_makefile_name = $$o.mak
+    nb_compile_each_file = T
+    nb_build_each_file = T
+    nb_makefile_in_start_in_dir = T
+    nb_proj_nmake_command =  
+    nb_proj_nmake_file =  
+    nb_proj_msdev_command =  
+    nb_proj_msdev_file =  
+    nb_projfile_in_start_in_dir = T
+    nb_proj_makefile_in_start_in_dir = T
+    nb_remove_pch = F
+    nb_substitute_source = F
+    sub_unused_files = F
+    nb_pipe = T
+    nb_force_console = 2
+    nb_object_extension = .O
+    enable_pre_build = 0
+    enable_post_build = 0
+    make_greenhills_bld = 0
+    nb_harness = "$$i$$x$$g"
+    nb_executepath = $(Userdir)
+    nb_execute_each_file = F
+    nb_pipe_exec = F
+    exhdir = $(Userdir)
+    modinst_exh_stem = $(SourceStem)_$(Modnum)
+    using_exhstrip = F
+    using_validexh = T
+    single_set_exh = F
+    compress_exh = F
+    threaded_exh = F
+    memory_address_exh = F
+    compress_exh_method = bitmap
+    do178b_instr = T
+    tbcapture_instr = F
+    array_bounds_check = F
+    copy_ref_instr = 0
+    archive_exh = T
+    force_dynsplit = T
+    dyn_scan_option = 6
+    dyn_scan_custom_coverage = 5 60 100 100 60 100
+    dyn_scan_current_coverage = 5 60 100 100 60 100
+    dyn_scan_html = T
+    dyn_oview_sort = 0
+    dyn_oview_options_changed = 0
+    dyn_oview_file_by_file = 0
+    dyn_oview_files_table = 0
+    syntax_only = F
+    include = T
+    open_all_includes = F
+    shorten = T
+    tbset_data_file = tbset.dat
+    workdir = $(WorkAreadir)tbwrkfls\
+    y2k_text_format = 1
+    y2k_column_width = 80
+    ddf_text_format = 1
+    ddf_column_width = 80
+    tbr_level = C:\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c 1
+    iprog = inszt_$$o
+    iext = $$t
+    idir = $(SourceDir)
+    exe_name = $$f
+    exe_append = .exe
+    cinstr_data_file = <Default>
+    cppinstr_data_file = <Default>
+    cpenfile = <Default>
+    cpppenfile = <Default>
+    cvalsfile = <Default>
+    cppvalsfile = <Default>
+    cglbfile = <Default>
+    cppglbfile = <Default>
+    cstandards_model = MISRA-C:2012
+    cexternal_standard = MISRA-C:2012
+    c_dialect = GCC
+    cppstandards_model = MISRA-C++:2008
+    cppexternal_standard = MISRA-C++:2008
+    cpp_dialect = GCC
+    application_accessed = Testbed
+    sysearch = <Default search - Source dir(s) - Installdir>
+    sysppvar = <Default search - Source dir(s) - Installdir>
+    dynamic_run_number = 1
+    history = 10
+    coverage = 3
+    exhwidth = 0
+    logging = F
+    thisdir = F
+    forcedataflow = T
+    dosnames = F
+    scanondemand = T
+    iprog_switch_os_format = F
+    iprog_flush_exh = F
+    preprocess_iprog = T
+    instr_template_io = F
+    current_compiler = MinGW200 GCC C/C++ v3.2
+    initial_compiler = MinGW200 GCC C/C++ v3.2
+
+    # End Options
+
+ # End Testbed Set
+ 
+ ..||||||| .r0
+=======
+
+ # Begin Testbed Set
+
+ SINGLE_FILE = TRUE
+ GENERATED_BY = Testbed 9.8.3
+ HOSTNAME = Imphal
+
+    # Begin Source Files
+
+    RelativeFile = .\Structs.c
+    File = C:\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c
+    UNCFile = \\Imphal\C\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c
+
+    # End Source Files
+
+    # Begin Sysearch Include File Entries
+
+
+    # End Sysearch Include File Entries
+
+    # Begin Sysppvar Preprocessor Macros
+
+
+    # End Sysppvar Preprocessor Macros
+
+    # Begin Options
+
+    set_attributes = 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    nb_mechanism = build
+    nb_start_in_dir = $(SourceDir)
+    nb_build_command = gcc "$$d$$f.$$e" -o "$$i$$x$$g"
+    nb_compile_command = gcc -c "$$d$$f.$$e"
+    nb_link_command = gcc $$d$$f.o -o "$$i$$x"
+    nb_ide_command = <Unknown TO GCC>
+    nb_makefile_command = mingw32-make -f "$(Makefile)" $(MakeGoal) $(MakeArgs)
+    nb_makefile_name = $$o.mak
+    nb_compile_each_file = T
+    nb_build_each_file = T
+    nb_makefile_in_start_in_dir = T
+    nb_proj_nmake_command =  
+    nb_proj_nmake_file =  
+    nb_proj_msdev_command =  
+    nb_proj_msdev_file =  
+    nb_projfile_in_start_in_dir = T
+    nb_proj_makefile_in_start_in_dir = T
+    nb_remove_pch = F
+    nb_substitute_source = F
+    sub_unused_files = F
+    nb_pipe = T
+    nb_force_console = 2
+    nb_object_extension = .O
+    enable_pre_build = 0
+    enable_post_build = 0
+    make_greenhills_bld = 0
+    nb_harness = "$$i$$x$$g"
+    nb_executepath = $(Userdir)
+    nb_execute_each_file = F
+    nb_pipe_exec = F
+    exhdir = $(Userdir)
+    modinst_exh_stem = $(SourceStem)_$(Modnum)
+    using_exhstrip = F
+    using_validexh = T
+    single_set_exh = F
+    compress_exh = F
+    threaded_exh = F
+    memory_address_exh = F
+    compress_exh_method = bitmap
+    do178b_instr = T
+    tbcapture_instr = F
+    array_bounds_check = F
+    copy_ref_instr = 0
+    archive_exh = T
+    force_dynsplit = T
+    dyn_scan_option = 6
+    dyn_scan_custom_coverage = 5 60 100 100 60 100
+    dyn_scan_current_coverage = 5 60 100 100 60 100
+    dyn_scan_html = T
+    dyn_oview_sort = 0
+    dyn_oview_options_changed = 0
+    dyn_oview_file_by_file = 0
+    dyn_oview_files_table = 0
+    syntax_only = F
+    include = T
+    open_all_includes = F
+    shorten = T
+    tbset_data_file = tbset.dat
+    workdir = $(WorkAreadir)tbwrkfls\
+    y2k_text_format = 1
+    y2k_column_width = 80
+    ddf_text_format = 1
+    ddf_column_width = 80
+    tbr_level = C:\983\LDRA_Workarea\Examples\C_tbrun_examples\Examples\Structs.c 1
+    iprog = inszt_$$o
+    iext = $$t
+    idir = $(SourceDir)
+    exe_name = $$f
+    exe_append = .exe
+    cinstr_data_file = <Default>
+    cppinstr_data_file = <Default>
+    cpenfile = <Default>
+    cpppenfile = <Default>
+    cvalsfile = <Default>
+    cppvalsfile = <Default>
+    cglbfile = <Default>
+    cppglbfile = <Default>
+    cstandards_model = MISRA-C:2012
+    cexternal_standard = MISRA-C:2012
+    c_dialect = GCC
+    cppstandards_model = MISRA-C++:2008
+    cppexternal_standard = MISRA-C++:2008
+    cpp_dialect = GCC
+    application_accessed = Testbed
+    sysearch = <Default search - Source dir(s) - Installdir>
+    sysppvar = <Default search - Source dir(s) - Installdir>
+    dynamic_run_number = 1
+    history = 10
+    coverage = 3
+    exhwidth = 0
+    logging = F
+    thisdir = F
+    forcedataflow = T
+    dosnames = F
+    scanondemand = T
+    iprog_switch_os_format = F
+    iprog_flush_exh = F
+    preprocess_iprog = T
+    instr_template_io = F
+    current_compiler = MinGW200 GCC C/C++ v3.2
+    initial_compiler = MinGW200 GCC C/C++ v3.2
+//comment added for id 725
+
