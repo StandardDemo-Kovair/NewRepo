a4e5201c48c564886b1c335913f3f21c8ecc486c
anshu_sinha <asinha@kovair.com>
2021-10-06 23:46:59 -0700

    761

--- DemoProjectNew/src/com/sonar/io/JavaProgram.java
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -16,7 +16,7 @@ public class JavaProgram {
 				System.out.println(a[i]);
 			}
 		}
-		//comment224
+	
 		
 		String input = "GeeksforGeeks"; 
 		  
@@ -35,7 +35,7 @@ public class JavaProgram {
   
         System.out.println(new String(result)); 
         System.out.println(new String(result)); 
-        // Requirement ID :2401
+        // Requirement ID :
 	
 	}
 	
