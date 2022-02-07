5c519276076c873edd802e6fb931ed39389bbad6
anshu_sinha <asinha@kovair.com>
2022-02-07 02:05:00 -0800

    78

--- DemoProjectNew/src/com/sonar/io/JavaProgram.java
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -34,7 +34,7 @@ public class JavaProgram {
   
         System.out.println(new String(result)); 
         System.out.println(new String(result)); 
-        // Requirement ID : 78
+        // Requirement ID : 786
 	}
 	
 }
