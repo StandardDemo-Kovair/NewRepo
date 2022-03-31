b2a5fff4da606e6b0f22aad810fc708b15282a08
anshu_sinha <asinha@kovair.com>
2022-03-31 00:28:34 -0700

    1018

--- DemoProjectNew/src/com/sonar/io/JavaProgram.java
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -34,7 +34,7 @@ public class JavaProgram {
   
         System.out.println(new String(result)); 
         System.out.println(new String(result)); 
-        // Requirement ID :1015
+        // Requirement ID :1018
 	}
 	
 }
